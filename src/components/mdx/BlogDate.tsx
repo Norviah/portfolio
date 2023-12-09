e4c87ff0fa8f6e501484cd71f3385975ea5108c5
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-09 03:45:25 -0800

    fix: render a blog's date on the client for accurate information

---
+++ src/components/mdx/BlogDate.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+function formatDate(date: string): string {
+  const currentDate: Date = new Date();
+  const targetDate: Date = new Date(date);
+
+  const yearsAgo: number = currentDate.getFullYear() - targetDate.getFullYear();
+  const monthsAgo: number = currentDate.getMonth() - targetDate.getMonth();
+  const daysAgo: number = currentDate.getDate() - targetDate.getDate();
+
+  let formattedDate: string = '';
+
+  if (yearsAgo > 0) {
+    formattedDate = `${yearsAgo}y ago`;
+  } else if (monthsAgo > 0) {
+    formattedDate = `${monthsAgo}mo ago`;
+  } else if (daysAgo > 0) {
+    formattedDate = `${daysAgo}d ago`;
+  } else {
+    formattedDate = 'Today';
+  }
+
+  const fullDate: string = targetDate.toLocaleString('en-us', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+
+  return `${fullDate} (${formattedDate})`;
+}
+
+export function BlogDate({ date }: { date: string }): JSX.Element {
+  return (
+    <p className="text-muted-foreground" suppressHydrationWarning>
+      {formatDate(date)}
+    </p>
+  );
+}
--- src/components/mdx/BlogHeader.tsx
+++ src/components/mdx/BlogHeader.tsx
@@ -1,5 +1,6 @@
+import { BlogDate } from '@/components/mdx/BlogDate';
+import { Separator } from '@/components/ui/Separator';
 import { Header } from '@/components/ui/typography/Header';
-import { Separator } from '../ui/Separator';
 
 import { cn } from '@/utils/cn';
 
@@ -9,35 +10,6 @@ interface BlogHeaderProps extends React.HTMLAttributes<HTMLDivElement> {
   blog: Blog;
 }
 
-function formatDate(date: string): string {
-  const currentDate: Date = new Date();
-  const targetDate: Date = new Date(date);
-
-  const yearsAgo: number = currentDate.getFullYear() - targetDate.getFullYear();
-  const monthsAgo: number = currentDate.getMonth() - targetDate.getMonth();
-  const daysAgo: number = currentDate.getDate() - targetDate.getDate();
-
-  let formattedDate: string = '';
-
-  if (yearsAgo > 0) {
-    formattedDate = `${yearsAgo}y ago`;
-  } else if (monthsAgo > 0) {
-    formattedDate = `${monthsAgo}mo ago`;
-  } else if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`;
-  } else {
-    formattedDate = 'Today';
-  }
-
-  const fullDate: string = targetDate.toLocaleString('en-us', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  });
-
-  return `${fullDate} (${formattedDate})`;
-}
-
 export function BlogHeader({ blog, className, ...props }: BlogHeaderProps): JSX.Element {
   return (
     <>
@@ -45,7 +17,7 @@ export function BlogHeader({ blog, className, ...props }: BlogHeaderProps): JSX.
         <Header type="h2">{blog.title}</Header>
         <div className="flex justify-between">
           <p className="text-muted-foreground">{blog.description}</p>
-          <p className="text-muted-foreground">{formatDate(blog.publishedAt)}</p>
+          <BlogDate date={blog.publishedAt} />
         </div>
       </div>
       <Separator className="my-4" />
