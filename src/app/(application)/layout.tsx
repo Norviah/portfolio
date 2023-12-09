03539cea44d25877826b2e0a3721dfd14a401262
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-09 04:56:26 -0800

    style: lessen padding for smaller screens

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -3,7 +3,7 @@ import type { LayoutProps } from '@/types/components/LayoutProps';
 
 export default function RootLayout({ children }: LayoutProps): JSX.Element {
   return (
-    <div className="mx-4 mb-40 mt-8 flex flex-col px-10 antialiased md:flex-row lg:mx-auto lg:max-w-3xl lg:px-0">
+    <div className="mx-4 mb-40 mt-8 flex flex-col px-5 antialiased md:flex-row lg:mx-auto lg:max-w-3xl lg:px-0">
       <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
         <Navbar />
         {children}
