385fb311b68343a0b0b1a2beee2fa63e94b7f945
Norviah <21983700+Norviah@users.noreply.github.com>
2023-12-11 18:26:17 -0800

    feat(components/ThemeSelector): set the tooltip's state to reflect if the mouse is hovering over the element

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -5,9 +5,11 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/comp
 import { MoonIcon, SunIcon } from 'lucide-react';
 
 import { useTheme } from 'next-themes';
+import { useState } from 'react';
 
 export function ThemeSelector(): JSX.Element {
   const { setTheme, theme: currentTheme } = useTheme();
+  const [openToolTip, setOpenToolTip] = useState(false);
 
   const themes = [
     {
@@ -28,8 +30,8 @@ export function ThemeSelector(): JSX.Element {
 
   return (
     <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
+      <Tooltip open={openToolTip}>
+        <TooltipTrigger asChild onMouseEnter={() => setOpenToolTip(true)} onMouseLeave={() => setOpenToolTip(false)}>
           <Button variant="icon" size="icon" onClick={theme.onclick}>
             <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
